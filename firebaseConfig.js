6ddaffde88c5d533a8f96b00d9670092dd96a09e
pooja.mishra <pooja.mishra@hestabit.in>
2022-10-13 16:50:05 +0530

    born

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgB6u74Q8ab6dREKKzSaj_mgvfZKavkQs",
+  authDomain: "crud-auth-dfe32.firebaseapp.com",
+  projectId: "crud-auth-dfe32",
+  storageBucket: "crud-auth-dfe32.appspot.com",
+  messagingSenderId: "195828813158",
+  appId: "1:195828813158:web:eafe4236a3baf939d1cca7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ layouts/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export const Layout = () =>{
+    <div>
+        <Link href={'/index'}></Link>
+        <Link href={'/register'}></Link>
+        <Link href={'/login'}></Link>
+   
+    </div>
+}
